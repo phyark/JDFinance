d69d36f98f47d9328fc5844fac594d2b242ac481
phyark <992060446@qq.com>
2019-09-17 20:32:24 +0800

    commit the basic files for the project

---
+++ src/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="scss" module>
+@import "../public/css/reset.scss";
+.home {
+  color: red;
+  font-size: 20px;
+}
+</style>
